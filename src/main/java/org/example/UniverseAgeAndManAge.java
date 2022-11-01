3b107565404bece0f4f2258d9f4dd765980e1e2c
OlyaGnatyuk <43353123+OlyaGnatyuk@users.noreply.github.com>
2022-11-01 14:57:52 +0300

    Initial commit

---
+++ src/main/java/org/example/UniverseAgeAndManAge.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class UniverseAgeAndManAge {
+    public static void main(String[] args) {
+        long universeAgeInYears = 13000000000L;
+        long manAgeInYears = 100000000L;
+        long rateBetweenUniverseAndManAges = universeAgeInYears / manAgeInYears;
+    }
+}
